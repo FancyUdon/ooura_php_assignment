e46c5e8efe63bb7bfe93e82db79ed74722c4da16
ネオ <pandaneo74@gmail.com>
2025-07-25 21:34:01 +0900

    think I complete my assignment PHP1-1
    I suppose I completed my assignment.
    For now, I push this changes, and after that, I confirm really did I completed it.

--- member_regist.php
+++ member_regist.php
@@ -21,57 +21,64 @@
     ?>
 
 
-    <h1>会員登録</h1>
-<form action="register_confirm.php" method="post">
+    <div class="form-container">
+        <h1>会員情報登録フォーム</h1>
+        <form action="register_confirm.php" method="post">
 
-    <div class="form-row">
-        <label>氏名:</label>
-        <div class="form-name">
-            <span>姓</span><input type="text" name="last_name" required>
-            <span>名</span><input type="text" name="first_name" required>
-        </div>
-    </div>
+                <div class="form-row">
+                    <label>氏名</label>
+                    <div class="form-name">
+                        <span>姓</span><input type="text" name="last_name" required>
+                        <span>名</span><input type="text" name="first_name" required>
+                    </div>
+                </div>
 
-        <label>性別: </label><br>
-        <label><input type="radio" name="gender" value="男性" required>男性</label><br>
-        <label><input type="radio" name="gender" value="女性">女性</label><br><br>
+                <div class="form-row">
+                    <label>性別</label>
+                    <div class="form-gender">
+                        <label><input type="radio" name="gender" value="男性" required>男性</label>
+                        <label><input type="radio" name="gender" value="女性">女性</label>
+                    </div>
+                </div>
 
-<div class="form-row-column">
-    <label>住所:</label>
-    <div class="address-group">
-        <div class="address-field-inline">
-            <label for="prefecture">都道府県</label>
-            <select name="prefecture" id="prefecture" required>
-                <option value="">選択してください</option>
-                <?php foreach ($prefectures as $pref): ?>
-                    <option value="<?= htmlspecialchars($pref, ENT_QUOTES, 'UTF-8') ?>"><?= htmlspecialchars($pref, ENT_QUOTES, 'UTF-8') ?></option>
-                <?php endforeach; ?>
-            </select>
-        </div>
-        <div class="address-field-below">
-            <label for="address_detail">それ以降の住所</label>
-            <input type="text" name="address_detail" id="address_detail" required>
-        </div>
-    </div>
-</div>
+            <div class="form-row-column">
+                <label>住所</label>
+                <div class="address-group">
+                    <div class="address-field-inline">
+                        <label for="prefecture">都道府県</label>
+                        <select name="prefecture" id="prefecture" required>
+                            <option value="">選択してください</option>
+                            <?php foreach ($prefectures as $pref): ?>
+                                <option value="<?= htmlspecialchars($pref, ENT_QUOTES, 'UTF-8') ?>"><?= htmlspecialchars($pref, ENT_QUOTES, 'UTF-8') ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="address-field-below">
+                        <label for="address_detail">それ以降の住所</label>
+                        <input type="text" name="address_detail" id="address_detail" required>
+                    </div>
+                </div>
+            </div>
 
-        <div class="form-row">
-            <label for="password">パスワード:</label>
+            <div class="form-row">
+            <label for="password">パスワード</label>
             <input type="password" id="password" name="password" required>
-        </div>
+            </div>
 
-        <div class="form-row">
-                <label for="password">パスワード(確認): </label>
+            <div class="form-row">
+                <label for="password">パスワード(確認) </label>
                 <input type="password" name="password_confirm" required>
-        </div>
+            </div>
 
-        <div class="form-row">
-                <label for="email">メールアドレス: </label>
+            <div class="form-row">
+                <label for="email">メールアドレス </label>
                 <input type="email" name="email" required>
-        </div>
+            </div>
 
 
-        <input type="submit" value="確認画面へ">
-    </form>
+            <div style="text-align:center; margin-top:20px;">
+            <button type="submit" class="button-confirm">確認画面へ</button>
+        </form>
+    </div>
 </body>
 </html>
